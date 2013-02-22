35b289ba63e9907b03efe0d3c8cc3055842816b0
Adie Tuk <adietuk@gmail.com>
2013-02-22 19:12:21 +0700

    Update Facebook Page/templates/base/web/views/modules/facebook_page/index.php

--- Facebook Page/templates/base/web/views/modules/facebook_page/index.php
+++ Facebook Page/templates/base/web/views/modules/facebook_page/index.php
@@ -1,9 +1,19 @@
-<?php
-
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+/**
+* TomatoCart Open Source Shopping Cart Solution
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License v3 (2007)
+* as published by the Free Software Foundation.
+*
+* @package TomatoCart
+* @author Adie Tuk
+* @copyright Copyright (c) 2013 Adie Tuk.
+* @license http://www.gnu.org/licenses/gpl.html
+* @link http://www.twitter.com/adietuk
+* @since Version 0.5.6
+* @filesource
+*/
 ?>
 <div class="box">
     <h4><?php echo $heading_title ?></h4>
